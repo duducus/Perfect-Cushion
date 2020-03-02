9f897b0b31923e8b9aea18a40ca31e81bb531eb4
Jorge Cerón <artic144@hotmail.com>
2020-03-01 19:37:50 -0600

    8. The Shop (part 4). Having problems with the database - by Cerón

--- perfectcushion/shop/views.py
+++ perfectcushion/shop/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Category, Product
 
 def index(request):
     text_var = 'This is my first Django App Web Page.'
     return HttpResponse(text_var)
+
+# category view
+
+def allProdCat(request, c_slug=None):
+    c_page = None
+    products = None
+    if c_slug != None:
+        c_page = get_object_or_404(Category, slug=c_slug)
+        products = Product.objects.filter(category=c_page,available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render (request, 'shop/category.html', {'category':c_page,'products':products})
